db90634f0d0994cda940a4b75b66cec390dcde83
baranka144 <baranka144@gmail.com>
2024-03-15 15:18:51 +0300

    3.3 + links

--- src/convConfToGram.ts
+++ src/convConfToGram.ts
@@ -15,20 +15,31 @@ type JSONContent = {
 	[key: string]: any;
 };
 
+function convertConfluenceParagraphNode(paragraphNode: JSONContent): JSONContent {
+	if (paragraphNode.marks) delete paragraphNode.marks; //убрать отступы в paragraph
+	return paragraphNode;
+}
+
 function convertConfluenceTextNode(textNode: JSONContent): JSONContent {
 	if (textNode.marks) {
-		textNode.marks = textNode.marks.filter((element) => element.type == "strong" || element.type == "em");
+		textNode.marks = textNode.marks.filter((element) => element.type == "strong" ||
+			element.type == "em" ||
+			element.type == "link");
 		if (textNode.marks.length == 0) delete textNode.marks;
 	};
 	return textNode;
 }
 
+function convertConfluenceHeadingNode(headingNode: JSONContent): JSONContent {
+	return headingNode;
+}
+
 function convertConfluenceBulletListNode(bulletListNode: JSONContent): JSONContent {
 	bulletListNode = {
-		type : "bullet_list",
-		attrs : { "tight" : false },
-		content : bulletListNode.content
-	}
+		type: "bullet_list",
+		attrs: { "tight": false },
+		content: bulletListNode.content
+	};
 	return bulletListNode;
 }
 
@@ -39,21 +50,96 @@ function convertConfluenceOrderedListNode(orderedListNode: JSONContent): JSONCon
 }
 
 function convertConfluenceListItemNode(listItemNode: JSONContent): JSONContent {
-	listItemNode.type = "list_item"
+	listItemNode.type = "list_item";
 	return listItemNode;
 }
 
+function convertConfluenceBlockQuoteNode(blockQuoteNode: JSONContent): JSONContent {
+	return blockQuoteNode;
+}
+
+function convertConfluenceExpandNode(expandNode: JSONContent): JSONContent {
+	expandNode.type = "cut";
+	expandNode.attrs = {
+		"text": expandNode.attrs["title"],
+		"expanded": "true",
+		"isInline": false
+	}
+	return expandNode;
+}
+
+function convertConfluenceCodeBlockNode(codeBlockNode: JSONContent): JSONContent {
+	codeBlockNode.type = "code_block";
+	codeBlockNode.attrs = {
+		"params": codeBlockNode.attrs["language"]
+	}
+	return codeBlockNode;
+}
+
+function convertConfluencePanelNode(panelNode: JSONContent): JSONContent {
+	panelNode.type = "note";
+	let type = "";
+	switch (panelNode.attrs["panelType"]) {
+		case "info":
+			type = "info";
+			break;
+		case "error":
+			type = "danger";
+			break;
+		case "note":
+			type = "tip";
+			break;
+		case "warning":
+			type = "note";
+			break;
+		default:
+			type = "info";
+			break;
+
+	}
+	panelNode.attrs = {
+		"type": type,
+		"title": "Заголовок"
+	}
+	return panelNode;
+}
+
+function convertConfluenceTableNode(tableNode: JSONContent): JSONContent {
+	if (tableNode.attrs) delete tableNode.attrs;
+	return tableNode;
+}
+
+function convertConfluenceTableRowNode(tableRowNode: JSONContent): JSONContent {
+	return tableRowNode;
+}
+
+function convertConfluenceTableHeaderNode(tableHeaderNode: JSONContent): JSONContent {
+	tableHeaderNode.attrs["colwidth"] = null;
+	return tableHeaderNode;
+}
+
+function convertConfluenceTableCellNode(tableCellNode: JSONContent): JSONContent {
+	tableCellNode.attrs["colwidth"] = null;
+	return tableCellNode;
+}
+
 function convertConfluenceNodeToGramaxNode(confluenceNode: JSONContent): JSONContent {
-	// Временно выделяю каждый элемент, чтобы мне нагляднее было
-	// Типы которые просто возвращаю без вызова функций их завернуть в функции на случай изменений или нет?
-	// Просто doc и paragraph довольно базовые, вот blockquote можно думаю
+	// завернуть ли doc в функцию
 	if (confluenceNode.type == "doc") return confluenceNode;
-	if (confluenceNode.type == "paragraph") return confluenceNode;
+	if (confluenceNode.type == "paragraph") return convertConfluenceParagraphNode(confluenceNode);
 	if (confluenceNode.type == "text") return convertConfluenceTextNode(confluenceNode);
+	if (confluenceNode.type == "heading") return convertConfluenceHeadingNode(confluenceNode);
 	if (confluenceNode.type == "bulletList") return convertConfluenceBulletListNode(confluenceNode);
 	if (confluenceNode.type == "orderedList") return convertConfluenceOrderedListNode(confluenceNode);
 	if (confluenceNode.type == "listItem") return convertConfluenceListItemNode(confluenceNode);
-	if (confluenceNode.type == "blockquote") return confluenceNode;
+	if (confluenceNode.type == "blockquote") return convertConfluenceBlockQuoteNode((confluenceNode));
+	if (confluenceNode.type == "expand") return convertConfluenceExpandNode((confluenceNode));
+	if (confluenceNode.type == "codeBlock") return convertConfluenceCodeBlockNode((confluenceNode));
+	if (confluenceNode.type == "panel") return convertConfluencePanelNode((confluenceNode));
+	if (confluenceNode.type == "table") return convertConfluenceTableNode((confluenceNode));
+	if (confluenceNode.type == "tableRow") return convertConfluenceTableRowNode((confluenceNode));
+	if (confluenceNode.type == "tableHeader") return convertConfluenceTableHeaderNode((confluenceNode));
+	if (confluenceNode.type == "tableCell") return convertConfluenceTableCellNode((confluenceNode));
 	return {};
 }
 
